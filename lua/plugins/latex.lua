4606d210862c3ee08b2bbd1befe032f75d45d059
AutumnKeesbury <angsukeesbury@gmail.com>
2026-01-19 15:33:56 -0500

    Zathura PDF!

--- lua/plugins/latex.lua
+++ lua/plugins/latex.lua
@@ -1,10 +1,10 @@
-return{
-    {
+return {
+  {
     "lervag/vimtex",
-        lazy = false,
-        init = function()
-        vim.cmd[[
-            let g:vimtex_view_method = 'skim'
+    lazy = false,
+    init = function()
+      vim.cmd [[
+            let g:vimtex_view_method = 'zathura'
             let g:vimtex_view_general_options = '-r @line @pdf @tex'
             let g:vimtex_compiler_method = 'latexmk'
             let g:vimtex_compiler_latexmk_engines = {
@@ -15,10 +15,10 @@ return{
                 \ 'xelatex-se' : '-xelatex --shell-escape',
                 \ 'bib-xelatex' : '-xelatex -biber',
            \}
-           ]] 
-        end,
-        dependencies = {
-        "AstroNvim/astrocore",
-        },
+           ]]
+    end,
+    dependencies = {
+      "AstroNvim/astrocore",
     },
+  },
 }
